747a1b1537e715e1a8f76f679d0f8d2a41c8888f
nggit <contact@anggit.com>
2022-06-24 06:03:24 +0700

    simplify mergeOptions

--- server.js
+++ server.js
@@ -377,7 +377,7 @@ const serverOnConnect = function(socket) {
 function mergeOptions(defaultOpts, options) {
   return Object.assign(
     Object.assign({}, defaultOpts),
-    Object.assign({}, options)
+    options
   );
 }
 
